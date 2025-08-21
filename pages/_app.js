90524e71f7254d7d30acdfb71612bfa0bd9f6340
webapp07 <aminezouguig@proton.me>
2025-08-21 12:06:37 -0400

    [200~- Added Redux Toolkit store configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.scss'
+import { Provider } from 'react-redux'
+import store from '../store'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
 
-function MyAp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+let persistor = persistStore(store)
+
+function MyApp({ Component, pageProps }) {
+
+
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  )
 }
 
 export default MyApp
