73e4f1c48ed4c5af74814f2fb70fe032ea9c2fb1
webapp07 <aminezouguig@proton.me>
2025-08-25 06:06:51 -0400

    create UserMenu component with conditional rendering for logged-in users, profile navigation links, and styled buttons (primary & outlined)

--- components/header/Top.js
+++ components/header/Top.js
@@ -4,8 +4,11 @@ import { MdSecurity } from "react-icons/md";
 import { RiAccountPinCircleLine } from "react-icons/ri";
 import { RiArrowDropDownFill } from "react-icons/ri";
 import Link from "next/link";
+import { useState } from "react";
+import UserMenu from "./UserMenu";
 
 export default function Top() {
+  const [loggdIn, setLoggedIn] = useState(false);
   return (
     <div className={styles.top}>
       <div className={styles.top__container}>
@@ -33,11 +36,27 @@ export default function Top() {
             </Link>
           </li>
           <li>
-            <div className={styles.flex}>
-              <RiAccountPinCircleLine />
-              <span>Account</span>
-              <RiArrowDropDownFill />
-            </div>
+            {loggdIn ? (
+              <li>
+                <div className={styles.flex}>
+                  <img
+                    src="https://as2.ftcdn.net/v2/jpg/03/64/21/11/1000_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+                    alt=""
+                  />
+                  <span>Amine</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            ) : (
+              <li>
+                <div className={styles.flex}>
+                  <RiAccountPinCircleLine />
+                  <span>Account</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            )}
+            <UserMenu loggdIn={loggdIn} />
           </li>
         </ul>
       </div>
---
+++ components/header/UserMenu.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+export default function UserMenu({ loggedIn }) {
+  return (
+    <div className={styles.menu}>
+      <h4>Welcome to Shoppay!</h4>
+      {loggedIn ? (
+        <div>
+          <img
+            src="https://as2.ftcdn.net/v2/jpg/03/64/21/11/1000_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+            alt=""
+            className={styles.menu__img}
+          />
+          <div>
+            <span>Welcome Back,</span>
+            <h3>ZOUGUIG</h3>
+            <span>Sign out</span>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.flex}>
+          <button className={styles.btn_primary}>Register</button>
+          <button className={styles.btn_outlined}>Login</button>
+        </div>
+      )}
+      <ul>
+        <li>
+          <Link href="/profile">Account</Link>
+        </li>
+        <li>
+          <Link href="/profile/orders">My Orders</Link>
+        </li>
+        <li>
+          <Link href="/profile/messages">Message Center</Link>
+        </li>
+        <li>
+          <Link href="/profile/address">Address</Link>
+        </li>
+        <li>
+          <Link href="/profile/wishlist">whishlist</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
