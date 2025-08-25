82b1d37d9932a8f1280d02e21a760cf97e05bcd0
webapp07 <aminezouguig@proton.me>
2025-08-25 10:11:26 -0400

    add Main component with logo, search bar, cart link and styles

---
+++ components/header/Main.js
@@ -0,0 +1,32 @@
+import { RiSearch2Line } from "react-icons/ri";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+import { FaOpencart } from "react-icons/fa";
+import { useSelector } from "react-redux";
+
+export default function Main() {
+  const { cart } = useSelector((state) => ({ ...state }));
+  return (
+    <div className={styles.main}>
+      <div className={styles.main__container}>
+        <Link href="/">
+          <a className={styles.logo}>
+            <img src="../../../logo.png" alt="" />
+          </a>
+        </Link>
+        <div className={styles.search}>
+          <input type="text" placeholder="Search..." />
+          <div className={styles.search__icon}>
+            <RiSearch2Line />
+          </div>
+        </div>
+        <Link href="/cart">
+          <a className={styles.cart}>
+            <FaOpencart />
+            <span>0</span>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- components/header/index.js
+++ components/header/index.js
@@ -1,12 +1,14 @@
 import Ad from "./Ad";
 import styles from "./styles.module.scss";
 import Top from "./Top";
+import Main from "./Main";
 
 export default function Header() {
   return (
     <div className={styles.header}>
       <Ad />
       <Top />
+      <Main />
     </div>
   );
 }
