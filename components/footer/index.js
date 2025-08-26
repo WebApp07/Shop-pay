a4d854136a46a90ab1ad2c20e7a69da8f996c27c
webapp07 <aminezouguig@proton.me>
2025-08-26 07:58:52 -0400

    implement Socials section with icons and Newsletter signup form

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,11 +1,15 @@
 import styles from "./styles.module.scss";
 import Links from "./Links";
+import Socials from "./Socials";
+import NewsLetter from "./NewsLetter";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__container}>
         <Links />
+        <Socials />
+        <NewsLetter />
       </div>
     </footer>
   );
